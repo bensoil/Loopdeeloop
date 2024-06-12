df46f65433d0f52414c084b61e3a3edae16b7755
bensoil <157688179+bensoil@users.noreply.github.com>
2024-06-12 10:15:23 +0200

    Add project files.

---
+++ Loopdeeloop/Program.cs
@@ -0,0 +1,47 @@
+﻿//OPPGAVE 1
+    void forLoop()
+    {
+        for (int i = 0; i < 5; i++) {
+
+        Console.WriteLine("Terje er kul");
+        
+        }
+    }
+
+forLoop();
+
+//OPPGAVE 2
+
+void forEachLoop()
+{
+    string bargs = "Hello";
+    foreach (char c in bargs)
+    {
+        Console.WriteLine(c);
+    }
+}
+
+forEachLoop();
+
+//OPPGAVE 3
+
+void WhileLoop(){
+
+    bool condish = true;
+int roundTime  = 0;
+
+while (condish)
+{
+    if (roundTime < 10)
+    {
+        Console.WriteLine($"Rundenummer: {roundTime}");
+            roundTime++;
+    } else
+    {
+        condish = false;
+    }
+
+}
+
+}
+WhileLoop();
